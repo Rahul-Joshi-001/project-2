5155fecccb038d56a676f727f5ea8a2d31a6dd66
Rahul Joshi <joshirahul3456@gmail.com>
2025-02-23 02:24:26 +0530

    new changes

--- java10.js
+++ java10.js
@@ -8,6 +8,7 @@ console.dir(btn);
 function say (){
     console.log("hello");
 }
+
 btn.onclick = say;
 
 btn.onmouseenter = function(){
@@ -18,7 +19,6 @@ btn.onmouseenter = function(){
 function say2 (){
     console.log("you are using event listner in this code !");
 
-    
 }
 
 let btn2 = document.querySelector("#btn2");
@@ -33,4 +33,76 @@ let para = document.querySelector("p");
 
 para.addEventListener("click",function(){
     console.log("para was clicked.!");
+    console.log(this);
+});
+
+let btn3 = document.querySelector("#btn3");
+
+
+btn3.addEventListener("click", function(){
+    console.log(this);
+});
+
+function changecolor(){
+    this.style.backgroundColor = "green";
+    console.log(this);
+}
+btn1.addEventListener("click",changecolor);
+btn2.addEventListener("click",changecolor);
+btn3.addEventListener("click",changecolor);
+
+
+let input = document.querySelector("input");
+
+input.addEventListener("keydown",function(){
+    console.log("key was pressed");
+});
+
+input.addEventListener("keyup",function(){
+    
+    console.log("key was released !");
 })
+
+let inp = document.querySelector("#inp");
+
+inp.addEventListener("keydown",function(event){
+    console.log("code = ",event.code);
+    if(event.code == "ArrowUp"){
+        console.log("character moves upward");
+    }
+    else if(event.code == "ArrowDown"){
+        console.log("character moves Down");
+    }
+    else if(event.code == "ArrowLeft"){
+        console.log("character moves left");
+    }
+    else if(event.code == "ArrowRight"){
+        console.log("character moves Right");
+    }
+
+    //can be done for keys but have to do keyU keyL etc...in == 
+})
+
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    let inpf = document.querySelector("#inpf");
+    console.log("username : ",inpf.value);
+    let inpp = document.querySelector("#inpp");
+    console.log("password : ",inpp.value);
+
+    alert(`hi ${inpf.value} your password is set to ${inpp.value} .`)
+})
+
+
+let text = document.querySelector("#text");
+let paraa = document.querySelector("#para");
+
+
+text.addEventListener("input",function(){
+console.log(text.value);
+paraa.innerText = text.value;
+} );
+
